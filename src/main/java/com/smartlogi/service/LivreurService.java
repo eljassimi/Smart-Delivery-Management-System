ed13411e9f82e82aa15a143801f38e9aafe60ecf
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-10-22 12:24:18 +0100

    Fit Livreur Service

---
+++ src/main/java/com/smartlogi/service/LivreurService.java
@@ -0,0 +1,49 @@
+package com.smartlogi.service;
+
+import com.smartlogi.entity.Livreur;
+import com.smartlogi.repository.LivreurRepository;
+import jakarta.transaction.Transactional;
+
+
+import java.util.List;
+import java.util.Optional;
+
+public class LivreurService {
+
+    private final LivreurRepository livreurRepository;
+
+    public LivreurService(LivreurRepository livreurRepository) {
+        this.livreurRepository = livreurRepository;
+    }
+
+    @Transactional
+    public Livreur creerLivreur(Livreur livreur) {
+        if (livreurRepository.existsByTelephone(livreur.getTelephone())) {
+            throw new RuntimeException("Un livreur avec ce telephone existe deja");
+        }
+        return livreurRepository.save(livreur);
+    }
+
+    public List<Livreur> obtenirTousLivreur(){
+        return livreurRepository.findAll();
+    }
+    public Optional<Livreur> obtenirLivreurParId(Long id){
+        return livreurRepository.findById(id);
+    }
+
+    @Transactional
+    public Livreur ModifierLivreur(Long id, Livreur livreurModifier){
+        Livreur livreur = livreurRepository.findById(id).orElseThrow(()->new RuntimeException("Livreur not found !"));
+
+        livreur.setNom(livreurModifier.getNom());
+        livreur.setPrenom(livreurModifier.getPrenom());
+        livreur.setTelephone(livreurModifier.getTelephone());
+
+        return livreurRepository.save(livreur);
+    }
+
+    @Transactional
+    public void SupprimerLivreur(Long id){
+        livreurRepository.deleteById(id);
+    }
+}
